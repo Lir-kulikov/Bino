e16b8ccde5e29b60104d05994a92c92b7d752cc6
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-09-30 11:40:52 +0300

    add close mobile menu after click on link, add smooth close mobile menu, remove border-radius inputs in safari

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,8 +1,19 @@
 const burger = document.querySelector('.mobile-menu');
 const scrollFixed = document.querySelector('body');
 const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
 
-burger.onclick = () => {
-    burger.classList.toggle("is-active");
-    nav.classList.toggle("is-active");
+burger.addEventListener('click', () => {
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+});
+
+for(i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('click', () => {
+        burger.classList.remove('is-active');
+        nav.classList.remove('is-active');
+    });
 }
+
+
+
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -6,53 +6,24 @@ const scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 const topBtn = document.querySelector('.scroll-to-top');
-const heroHeight = document.querySelector('.hero').offsetHeight;
 const top = document.querySelector('.header');
 
 // обновление значений при ресайзе
 
-// window.onresize = () => {heroHeight = document.querySelector('.hero').offsetHeight}
+window.addEventListener('resize', () => {
+  showScroll();
+});
+window.addEventListener('scroll', () => {
+  showScroll();
+});
 
 // показ и скрытие кнопки скролла наверх
 
-window.onscroll = (e) => {
+function showScroll() {
+  let heroHeight = document.querySelector('.hero').offsetHeight;
   if (window.scrollY > heroHeight) {
     document.querySelector('.scroll-to-top').classList.add('is-show');
   } else {
     document.querySelector('.scroll-to-top').classList.remove('is-show');
   }
 }
-
-// плавный скролл наверх
-
-// for (const link of links) {
-//   link.addEventListener('click', clickHandler);
-// }
-
-// topBtn.addEventListener('click', clickHandler);
-
-// function clickHandler(e) {
-//   e.preventDefault();
-//   const href = this.getAttribute('href');
-//   const offsetTop = document.querySelector(href).offsetTop;
- 
-//   scroll({
-//     top: offsetTop - 100,
-//     behavior: 'smooth'
-//   });
-// }
-
-// const anchors = document.querySelectorAll('a[href*="#"]')
-
-// for (let anchor of anchors) {
-//   anchor.addEventListener('click', function (e) {
-//     e.preventDefault()
-    
-//     const blockID = anchor.getAttribute('href').substr(1)
-    
-//     document.getElementById(blockID).scrollIntoView({
-//       behavior: 'smooth',
-//       block: 'start'
-//     })
-//   })
-// }
