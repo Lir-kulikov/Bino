9346a8cb7c8299c20182cc2f045f9e186bb210e5
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-10-08 23:44:57 +0300

    добавляет полифилл nodelist foreach

--- src/js/polyfills.js
+++ src/js/polyfills.js
@@ -54,3 +54,12 @@ if (!Array.prototype.forEach) {
     // 8. Вернём undefined.
   };
 }
+
+if (window.NodeList && !NodeList.prototype.forEach) {
+  NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+          callback.call(thisArg, this[i], i, this);
+      }
+  };
+}
