5dcfbbc063107208e47454da9ffa424ae428e134
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-10-09 23:29:20 +0300

    change polyfill

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -50,18 +50,18 @@ function thxModalForm () {
 const float = document.querySelectorAll('.js-floating');
 const field = document.querySelectorAll('.form-contacts__input');
 
-// function floatingLabels () {
-//     for (let i = 0; i < field.length; i++) {
-//         field[i].addEventListener('focus', () => float[i].classList.add('is-floating'));
-//         field[i].addEventListener('blur', () => {
-//             if (field[i].value == 0) {
-//                 float[i].classList.remove('is-floating');
-//             }
-//         });
-//     }
-// }
+function floatingLabels () {
+    for (let i = 0; i < field.length; i++) {
+        field[i].addEventListener('focus', () => float[i].classList.add('is-floating'));
+        field[i].addEventListener('blur', () => {
+            if (field[i].value == 0) {
+                float[i].classList.remove('is-floating');
+            }
+        });
+    }
+}
 
-// floatingLabels ();
+floatingLabels ();
 
 const form = document.querySelector(".form-contacts");
 import Bouncer from '../js/vendor/bouncer.polyfills.min';
@@ -71,19 +71,19 @@ var validate = new Bouncer('.form-contacts', {
 });
 
 document.addEventListener('bouncerFormValid', function () {
-    //alert('Form submitted successfully!');
-    // thxModalForm();
-    // for (let elem of form.elements) {
-    //     if (
-    //         !elem.classList.contains("btn") && !elem.classList.contains("form-thx")
-    //     ) {
-    //         elem.value = "";
+    alert('Form submitted successfully!');
+    thxModalForm();
+    for (let elem of form.elements) {
+        if (
+            !elem.classList.contains("btn") && !elem.classList.contains("form-thx")
+        ) {
+            elem.value = "";
 
-    //         for (let i = 0; i < float.length; i++) {
-    //             float[i].classList.remove('is-floating');
-    //         }
-    //     }
-    // }
+            for (let i = 0; i < float.length; i++) {
+                float[i].classList.remove('is-floating');
+            }
+        }
+    }
 }, false);
 
 
