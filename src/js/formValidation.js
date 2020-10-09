b836e88fa3b1e92e7739034c09aa6e200373e693
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-10-09 23:18:55 +0300

    add iterator polyfill

--- src/js/formValidation.js
+++ src/js/formValidation.js
@@ -34,7 +34,7 @@
 // });
 
 // модальное окно
-
+import "../../node_modules/iterators-polyfill/polyfill.min"
 function thxModalForm () {
     document.querySelector('.form-thx').classList.add('is-open');
     setTimeout(() => {
@@ -50,18 +50,18 @@ function thxModalForm () {
 const float = document.querySelectorAll('.js-floating');
 const field = document.querySelectorAll('.form-contacts__input');
 
-function floatingLabels () {
-    for (let i = 0; i < field.length; i++) {
-        field[i].addEventListener('focus', () => float[i].classList.add('is-floating'));
-        field[i].addEventListener('blur', () => {
-            if (field[i].value == 0) {
-                float[i].classList.remove('is-floating');
-            }
-        });
-    }
-}
+// function floatingLabels () {
+//     for (let i = 0; i < field.length; i++) {
+//         field[i].addEventListener('focus', () => float[i].classList.add('is-floating'));
+//         field[i].addEventListener('blur', () => {
+//             if (field[i].value == 0) {
+//                 float[i].classList.remove('is-floating');
+//             }
+//         });
+//     }
+// }
 
-floatingLabels ();
+// floatingLabels ();
 
 const form = document.querySelector(".form-contacts");
 import Bouncer from '../js/vendor/bouncer.polyfills.min';
@@ -72,18 +72,18 @@ var validate = new Bouncer('.form-contacts', {
 
 document.addEventListener('bouncerFormValid', function () {
     //alert('Form submitted successfully!');
-    thxModalForm();
-    for (let elem of form.elements) {
-        if (
-            !elem.classList.contains("btn") && !elem.classList.contains("form-thx")
-        ) {
-            elem.value = "";
+    // thxModalForm();
+    // for (let elem of form.elements) {
+    //     if (
+    //         !elem.classList.contains("btn") && !elem.classList.contains("form-thx")
+    //     ) {
+    //         elem.value = "";
 
-            for (let i = 0; i < float.length; i++) {
-                float[i].classList.remove('is-floating');
-            }
-        }
-    }
+    //         for (let i = 0; i < float.length; i++) {
+    //             float[i].classList.remove('is-floating');
+    //         }
+    //     }
+    // }
 }, false);
 
 
