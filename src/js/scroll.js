f08485cc0eb42b1b1444f46ca0d680a515c8f375
Kirill Kulikov <58344309+Lir-kulikov@users.noreply.github.com>
2020-10-10 15:36:11 +0300

    add smooth-scroll library

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,8 +1,37 @@
 //import smoothscroll from 'smoothscroll-polyfill';
-import smoothscrollPolyfill from 'smoothscroll-polyfill';
-import smoothscrollAnchorPolyfill from 'smoothscroll-anchor-polyfill';
-smoothscrollPolyfill.polyfill();
+// import smoothscrollPolyfill from 'smoothscroll-polyfill';
+// import smoothscrollAnchorPolyfill from 'smoothscroll-anchor-polyfill';
+// smoothscrollPolyfill.polyfill();
 // обновление значений при ресайзе
+
+// function smoothScroll (target, duration) {
+//   let target = document.querySelector(target);
+//   let targetPosition = target.getBoundingClientRect().top;
+//   let startPosition = window.pageYOffset;
+//   let distance = targetPosition - startPosition
+
+//   function animation (currentTime) {
+//     if (startTime === null) startTime = currentTime;
+//     let timeElapsed = currentTime - startTime;
+//     let run = ease(timeElapsed, startPosition, distance, duration);
+//     window.scrollTo(0, run);
+//     if (timeElapsed < duration) requestAnimationFrame(animation);
+//     console.log('timeElapsed : ' + timeElapsed + 'duration: ' + duration);
+//   }
+
+//   function ease (t, b, c, d) {
+//     t /= d / 2;
+//     if (t < 1) return c / 2 * t * t + b;
+//     t--;
+//     return -c / 2 * (t * (t - 2) -1) + b;
+//   }
+
+//   requestAnimationFrame(animation);
+// }
+
+import SmoothScroll from 'smooth-scroll';
+
+var scroll = new SmoothScroll('a[href*="#"]');
 
 window.addEventListener('resize', () => {
   showScroll();
